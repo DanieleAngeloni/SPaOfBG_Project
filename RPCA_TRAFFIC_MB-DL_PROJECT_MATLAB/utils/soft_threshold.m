0b7522b44437f79c8d8fa9da6a3ba05ff31dcdc1
DanieleAngeloni <122454079+DanieleAngeloni@users.noreply.github.com>
2026-02-13 16:52:30 +0100

    Delete RPCA_TRAFFIC_MB-DL_PROJECT_MATLAB directory

--- RPCA_TRAFFIC_MB-DL_PROJECT_MATLAB/utils/soft_threshold.m
+++
@@ -1,3 +0,0 @@
-function Y = soft_threshold(X, tau)
-Y = sign(X) .* max(abs(X) - tau, 0);
-end
--- RPCA_TRAFFIC_MB-DL_PROJECT_MATLAB/utils/svt_operator.m
+++
@@ -1,6 +0,0 @@
-function L = svt_operator(X, tau)
-[U,S,V] = svd(X, 'econ');
-s = diag(S);
-s = max(s - tau, 0);
-L = U * diag(s) * V';
-end
